fcfaa4d8c8123837e2bc3cb9eec683abb5ff7791
Robert Grahovac <robert.grahovac@hotmail.com>
2018-10-29 10:49:06 +0100

    smiley 2

--- draw.js
+++ draw.js
@@ -5,24 +5,29 @@ function draw() {
    
     context.lineWidth = 5;
     context.strokeStyle = "blue";
-    
+
+  
     context.beginPath();
+    drawHead(context);
+    drawEyes(context);
+    drawMouth(context);
+    context.stroke();
+}
+
+function drawHead(context)  {
     context.arc(310, 310, 300, 0, 2*Math.PI);
-    context.stroke();
+}
 
-    context.beginPath();
+function drawEyes(context)  {
+    context.moveTo(260, 210);
     context.arc(210, 210, 50, 0, 2*Math.PI);
-    context.stroke();
 
-    context.beginPath();
+    context.moveTo(460, 210)
     context.arc(410, 210, 50, 0, 2*Math.PI);
-    context.stroke();
+}
 
-    context.beginPath();
+function drawMouth(context)  {
+    context.moveTo();
     context.arc(310, 310, 200, 0.5, 0.8*Math.PI);
-    context.stroke();
 
-
-    
-    
 }
